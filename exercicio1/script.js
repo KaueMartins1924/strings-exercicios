13a79689876c4aebf80299bddc94fad3f3cdb8b1
KaueMartins1924 <kaue.silvamartinsshopee@gmail.com>
2023-11-14 14:04:46 -0300

    exercicio 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,8 @@
 function imprimirComidasFavoritas (){
-
+    const nome = (prompt("Qual seu nome?"))
+    const comida1 = (prompt("Qual sua comida favorita?"))
+    const comida2 = (prompt("Qual sua segunda comida favorita?"))
+    const comida3 = (prompt("Qual sua terceira comida favorita?"))
+    console.log ("Estas são as comidas favoritas de", nome, "é:", "\n1=",comida1, "\n2=",comida2, "\n3=",comida3)
 }
+imprimirComidasFavoritas()
