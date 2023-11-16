177f66defbfe4bfcdd543dab935b4a9b30c3575b
KaueMartins1924 <kaue.silvamartinsshopee@gmail.com>
2023-11-16 09:01:41 -0300

    exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
-//Crie a const para a frase aqui
+
+function exercicio3(){
+    const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+    const substituir = frase.replace("verde","rosa")
+    const substituir2 = substituir.replace ("azul", "laranja")
+    const maiusculo = substituir2.toUpperCase("mas não deixe o gato sair")
+    console.log ("Frase original:",frase,"\nFrase alterada:",substituir2,"\nFrase com final maiúsculo", maiusculo)
+}
+exercicio3()
